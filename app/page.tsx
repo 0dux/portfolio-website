3644c211070bd1ce246487e7c061077af03eb479
Daksh Yadav <dakshdav@gmail.com>
2026-04-16 21:49:38 +0530

    building the navbar for the portfolio and also the logo

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,17 @@
 import Container from "@/components/common/Container";
+import Navbar from "@/components/common/Navbar";
 
 const page = () => {
-  return <Container>random</Container>;
+  return (
+    <Container>
+      <div //why does this w-7xl overflow the container width?
+        className="w-full"
+      >
+        random ass shit
+        <Navbar />
+      </div>
+    </Container>
+  );
 };
 
 export default page;
--- components/common/Container.tsx
+++ components/common/Container.tsx
@@ -6,12 +6,17 @@ export default function Container({
   children,
   ...props
 }: React.ComponentProps<"div"> & {
-  //intersection combines both the properties, component props div - tells what elements props it can have, increased clarity no necessary to give though just for more typesafety
+  //what is React.ComponentProps and its purpose?
+  //why are we using "&" here and what does it do generally?
   className?: string;
-  children: React.ReactNode; // used for refering to the child elements.
+  children: React.ReactNode; //what is React.ReactNode and some common must know examples like this?
 }) {
   return (
-    <div className={cn("mx-auto px-4 max-w-3xl", className)} {...props}>
+    //why do we use cn instead of just using `some random tailwind code ${className}`
+    <div
+      className={cn("mx-auto px-4 max-w-3xl container", className)}
+      {...props} //how does props work here and why was it used here?
+    >
       {children}
     </div>
   );
