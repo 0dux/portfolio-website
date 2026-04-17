a8d40a2616e3e3160c4f3d3974f444ad3ea442ec
Daksh Yadav <dakshdav@gmail.com>
2026-04-17 15:12:35 +0530

    navbar in progress

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,11 @@
 import Container from "@/components/common/Container";
-import Navbar from "@/components/common/Navbar";
 
 const page = () => {
   return (
     <Container>
       <div //why does this w-7xl overflow the container width?
         className="w-full"
-      >
-        random ass shit
-        <Navbar />
-      </div>
+      ></div>
     </Container>
   );
 };
--- components/common/Navbar.tsx
+++ components/common/Navbar.tsx
@@ -1,5 +1,34 @@
+import Image from "next/image";
+import Link from "next/link";
+import { BlocksIcon } from "../animate-ui/icons/blocks";
+import { AnimateIcon } from "../animate-ui/icons/icon";
+
 const Navbar = () => {
-  return <nav>Navbar</nav>;
+  return (
+    <nav className="max-w-3xl mx-auto px-4 container flex justify-between py-2">
+      <div className="flex items-center justify-center space-x-8">
+        <Link href={"/"}>
+          <Image
+            className="rounded-full h-12 w-12 border border-gray-200 hover:scale-90 hover:border-2 ease-in-out transition-all"
+            src={"/assets/nav_logo.jpg"}
+            width={100}
+            height={100}
+            alt="Home"
+          />
+        </Link>
+        <AnimateIcon asChild animateOnHover>
+          <Link
+            className="flex items-center justify-center gap-2 group"
+            href={"/projects"}
+          >
+            <BlocksIcon className={"w-4 h-4"} /> Projects
+          </Link>
+        </AnimateIcon>
+        <Link href={"/resume"}>Resume</Link>
+      </div>
+      <div></div>
+    </nav>
+  );
 };
 
 export default Navbar;
